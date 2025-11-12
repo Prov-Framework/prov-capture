0af88e049731d35251bdbd6c8e51369a0b1478fa
Ryan <ryan.gross.01@gmail.com>
2025-11-12 08:17:14 -0500

    Added test to push kafka messages.

--- src/test/java/com/provframework/capture/CaptureApplicationTests.java
+++ src/test/java/com/provframework/capture/CaptureApplicationTests.java
@@ -1,13 +1,46 @@
 package com.provframework.capture;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class CaptureApplicationTests {
 
+	@Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
 	@Test
-	void contextLoads() {
+	void listen() {
+		String bundle = """
+				{
+					"id": "bundle 1",
+					"entities": [ {
+						"id": "Entity 2",
+						"wasDerivedFrom": [ {
+							"id": "Entity 1"
+						}],
+						"wasAttributedTo": [ {
+							"id": "Person Agent",
+							"actedOnBehalfOf": [ {
+								"id": "Organization Agent"
+							}]
+						}],
+						"wasGeneratedBy": [ {
+							"id": "Activity 2",
+							"wasAssociatedWith": [ {
+								"id": "Software Agent"
+							}],
+							"used": [ {
+								"id": "Entity 1"
+							}]
+						}]
+					}]
+				}
+				""";
+
+		kafkaTemplate.send("prov", bundle);
 	}
 
 }
