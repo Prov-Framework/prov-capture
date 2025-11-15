a265b0358cbcfd07c7c6d67114af74cb7626f806
Ryan <ryan.gross.01@gmail.com>
2025-11-15 06:49:36 -0500

    Moving core message processing logic into main.

---
+++ src/main/java/com/provframework/capture/Main.java
@@ -0,0 +1,29 @@
+package com.provframework.capture;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.provframework.capture.language.Sparql;
+import com.provframework.capture.model.Bundle;
+
+@SpringBootApplication
+@EnableKafka
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.groupId}")
+	public void listen(Bundle bundle) {
+		bundle.setId(UUID.randomUUID().toString());
+		bundle.setGeneratedAtTime(Instant.now().toEpochMilli());
+
+		String statement = Sparql.generateStatement(bundle);
+	}
+
+}
--- src/main/java/com/provframework/capture/kafka/Listener.java
+++ src/main/java/com/provframework/capture/kafka/Listener.java
@@ -1,27 +1,21 @@
 package com.provframework.capture.kafka;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import com.provframework.capture.model.Bundle;
 
 @Service
 public class Listener {
-
-	@KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.groupId}")
-	public void listen(Bundle bundle) {
-		System.out.println(bundle.getId());
-	}
-
 	@Bean
 	public ConcurrentKafkaListenerContainerFactory<String, Bundle> kafkaListenerContainerFactory(
-			ConsumerFactory<String, Bundle> consumerFactory) {
+			@NonNull ConsumerFactory<String, Bundle> consumerFactory) {
 		ConcurrentKafkaListenerContainerFactory<String, Bundle> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
 		return factory;
 	}
-    
+
 }
